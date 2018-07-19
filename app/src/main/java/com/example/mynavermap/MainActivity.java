96a0c0ed68eda06e3f20b88f2a74f4017df22249
dbwlsl97 <kimyjin0920@gmail.com>
2018-07-19 21:28:48 +0900

    캡스톤 1학기내용

---
+++ app/src/main/java/com/example/mynavermap/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mynavermap;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.nhn.android.maps.mapcore.NMapDataProvider;
+import com.nhn.android.maps.overlay.NMapPathData;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+//        String url = "http://m.map.naver.com/route.nhn?menu=route&sx=126.9816485&sy=37.4765675&ex=127.0276368&ey=37.4979502 ";
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//        startActivity(intent);
+
+//        mMapLocationManager = new NMapLocationManager(SettingActivity.this);
+//        mMapLocationManager
+//                .setOnLocationChangeListener(onMyLocationChangeListener);
+//        mMapLocationManagerenableMyLocation(true);
+
+
+    }
+}
